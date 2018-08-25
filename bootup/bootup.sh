e6fe22d355d26354f0170247cc2aeb3c53fc4761
uzulla <uzulla@himitsukichi.com>
2018-08-26 03:04:12 +0900

    vsd破損時のハマりを避ける

--- bootup/bootup.sh
+++ bootup/bootup.sh
@@ -110,12 +110,19 @@ set -e
 IPADDR_LIST=`ip -f inet -o addr show |grep -v 127.0.0.1 |cut -d\  -f 2,7 |cut -d/ -f 1`
 COMMIT_ID=`git -C /home/pi/bcon_nafuda log --oneline |head -1`
 
+DEFAULT_PASSWORD="{default_passwd.txt missing}"
+if [ -e "${VSD_BASE_DIR}/default_passwd.txt" ]
+then
+    DEFAULT_PASSWORD=`cat ${VSD_BASE_DIR}/default_passwd.txt`
+fi
+
+
 STARUP_TEXT="NAFUDA
 commit: ${COMMIT_ID}
 
 =INFO=
 ip: ${IPADDR_LIST}
-default password: `cat ${VSD_BASE_DIR}/default_passwd.txt`
+default password: ``
 usb gadget mode: ${G_MODE}
 
 ======
