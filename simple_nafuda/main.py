00035efcf83030d251922c5694ef7e180bd6dc8e
uzulla <uzulla@himitsukichi.com>
2018-08-29 20:47:50 +0900

    WIP

--- simple_nafuda/main.py
+++ simple_nafuda/main.py
@@ -29,6 +29,15 @@ import os
 import sys
 import time
 import re
+import qrcode
+import hashlib
+import requests
+import json
+import traceback
+from PIL import Image
+
+# BASE_URL = "https://eb2018.builderscon.io/"
+BASE_URL = "http://u.cfe.jp/"
 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../lib'))
 
@@ -37,18 +46,79 @@ from nafuda import Nafuda
 if "IMG_DIR" in os.environ:
     IMG_DIR = os.environ["IMG_DIR"]
 else:
-    if os.path.exists('/mnt/virtual_sd/img/'):
-        IMG_DIR = '/mnt/virtual_sd/img/'
+    if os.path.isdir('/mnt/virtual_sd/img'):
+        IMG_DIR = '/mnt/virtual_sd/img'
     else:
+        # virtual sdがない時（開発時など）用
         IMG_DIR = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/img')
+
+CLOUD_JSON_CACHE_PATH = IMG_DIR + "/cloud.json"
+CLOUD_QR_CODE_FILE_NAME = "__CONVERT_TO_QR__.png"
+CLOUD_QR_CODE_FILE_PATH = IMG_DIR + "/" + CLOUD_QR_CODE_FILE_NAME
 
 
 def main():
+    # try get images from cloud.
+    # QRコードに置換する画像がなければ、クラウド機能は不要と判断して実行しない
+    if os.path.isfile(CLOUD_QR_CODE_FILE_PATH):
+        try:
+            # 設定JSONをサーバーから取得してみる
+            r = requests.get(get_control_url() + "json")
+            if r.status_code != 200:
+                raise ValueError("json not found")
+
+            json_str = r.text
+
+            data = json.loads(json_str)
+            img_list = data['list']
+            if not isinstance(img_list, list):
+                raise ValueError("maybe invalid json")
+
+            if len(img_list) == 0:
+                raise ValueError("empty list")
+
+            # 過去のJSONと一致するか？
+            if os.path.isfile(CLOUD_JSON_CACHE_PATH):
+                with open(CLOUD_JSON_CACHE_PATH, "r") as jc:
+                    cached_json = jc.read()
+                    if cached_json == json_str:
+                        raise ValueError("json not updated")
+
+            # rwで再マウント
+            if os.path.isfile("/usr/bin/mount_vsd_rw"):
+                os.system('sudo mout_vsd_rw')
+
+            # clean up img dir
+            file_list_to_rm = os.listdir(IMG_DIR)
+            for f in file_list_to_rm:
+                p = IMG_DIR+"/"+f
+                if os.path.isfile(p):
+                    if re.search('^[^\.].*\.(png|jpg|jpeg|gif)', p, re.IGNORECASE):
+                        if f != CLOUD_QR_CODE_FILE_NAME:
+                            os.remove(p)
+
+            id = 1
+            for img in img_list:
+                root, ext = os.path.splitext(img)
+                get_and_save_file(get_control_url()+"/"+img, IMG_DIR+"/"+str(id)+ext)
+                id = id + 1
+
+            # save json
+            with open(CLOUD_JSON_CACHE_PATH, "w") as jc:
+                jc.write(json_str)
+
+            # roで再マウント
+            if os.path.isfile("/usr/bin/mount_vsd_ro"):
+                os.system('sudo mout_vsd_ro')
+
+        except:
+            traceback.print_exc()
+
     # load image file list
     file_list = []
     for file in os.listdir(IMG_DIR):
         # get (png|jpg|jpeg|gif) files. and skip dot files.
-        if re.search('^[^\.].+\.(png|jpg|jpeg|gif)', file, re.IGNORECASE):
+        if re.search('^[^\.].*\.(png|jpg|jpeg|gif)', file, re.IGNORECASE):
             file_list.append(file)
 
     if len(file_list) == 0:
@@ -63,6 +133,14 @@ def main():
 
     while True:
         for file in file_list:
+            if file == CLOUD_QR_CODE_FILE_NAME:
+                # QRコードを合成して表示
+                base_image = Image.open(CLOUD_QR_CODE_FILE_PATH)
+                qr_img = get_control_url_qrcode_img()
+                base_image.paste(qr_img, (10, 10))
+                nafuda.draw_image_buffer(base_image, orientation=90)
+                continue
+
             try:
                 nafuda.draw_image_file(IMG_DIR + '/' + file, 90)
 
@@ -77,6 +155,53 @@ def main():
             # 一枚しか画像がなければ、スライドショーする意味がないので終了
             if len(file_list) == 1:
                 exit(0)
+
+
+def get_nafuda_id():
+    h_path = '/mnt/virtual_sd/default_hostname.txt'
+    p_path = '/mnt/virtual_sd/default_passwd.txt'
+
+    if not os.path.isfile(h_path) or not os.path.isfile(p_path):
+        return False
+
+    try:
+        hostname = open(h_path).read(128).encode('UTF-8')
+        passwd = open(p_path).read(128).encode('UTF-8')
+
+        hash = hashlib.sha256(hostname + passwd).hexdigest()
+        # print(hash)
+
+    except OSError:
+        return False
+
+    return hash
+
+
+def get_control_url():
+    return BASE_URL + get_nafuda_id() + "/"
+
+
+def get_control_url_qrcode_img():
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=6,
+        border=4,
+    )
+
+    qr.add_data(get_control_url())
+    qr.make(fit=True)
+    # print(qr.size)
+    return qr.make_image(fill_color="black", back_color="white")
+
+
+def get_and_save_file(url, file_path):
+    r = requests.get(url, stream=True)
+    with open(file_path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
 
 
 if __name__ == '__main__':
